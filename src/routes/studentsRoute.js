a6ed46508f1c64ecc499a79daf6d2fc5711d9bb5
noha_isaed <thahernoha@gmail.com>
2022-05-27 20:41:34 +0300

    end the table

--- src/routes/studentsRoute.js
+++ src/routes/studentsRoute.js
@@ -10,6 +10,7 @@ router.post("/login", studentsController.checkLoginInformation);
 router.get("/table/:problemType/:status", studentsController.getStudentsProblem);
 router.get("/table_with_status/:status/:problemType", studentsController.getStatus);
 router.put("/table", studentsController.changeStatus);
+router.post("/sendEmail", studentsController.sendEmail);
 
 
 
